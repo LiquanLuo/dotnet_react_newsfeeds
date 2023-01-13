0d2ebafdd193d7adcc24ad459c1b5a36c5ff6ba2
liquan1que1@gmail.com <liquan1que1@gmail.com>
2023-01-14 09:45:41 +1100

    Add security headers

--- API/Program.cs
+++ API/Program.cs
@@ -24,12 +24,38 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseXContentTypeOptions();   // protect from MIME-sniffing
+app.UseReferrerPolicy(opt => opt.NoReferrer());
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+app.UseXfo(opt => opt.Deny());  // protect from Clickjacking
+app.UseCsp(opt => opt
+  .BlockAllMixedContent()
+  .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+  .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+  .FormActions(s => s.Self())
+  .FrameAncestors(s => s.Self())
+  .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
+  .ScriptSources(s => s.Self())
+);
+
+
+
+
 // read from appsettings.Development.json
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
+}
+
 app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 
